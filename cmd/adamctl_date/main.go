a4a6fc686fa62fb8aa7735e6dcc3aa9958244579
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2023-06-17 11:38:14 -0700

    feat: Print date observed by places (#2)
    * feat: Print time as observed by various cities
    
    * refactor: tweak output formatting
    
    * refactor: DRY up location creation
    
    * refactor: they are places, not cities

--- cmd/adamctl_date/main.go
+++ cmd/adamctl_date/main.go
@@ -7,8 +7,46 @@ import (
 	"time"
 )
 
+type place struct {
+	Name     string
+	Location *time.Location
+}
+
+func getPlaces() (*[]place, error) {
+	desired := [](struct {
+		Display  string
+		Timezone string
+	}){
+		{"Los Angeles", "America/Los_Angeles"},
+		{"Denver", "America/Denver"},
+		{"New York", "America/New_York"},
+		{"Turkey", "Turkey"},
+	}
+
+	cities := make([]place, 0)
+	for _, city := range desired {
+		location, err := time.LoadLocation(city.Timezone)
+		if err != nil {
+			return nil, err
+		}
+		cities = append(cities, place{city.Display, location})
+	}
+
+	return &cities, nil
+}
+
 func run(out io.Writer, t time.Time) {
-	fmt.Fprintln(out, t.Format(time.UnixDate))
+	places, err := getPlaces()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v\n", *places)
+	fmt.Fprintf(out, "%v\n\n", t.Format(time.UnixDate))
+	fmt.Fprintf(out, "UTC: %v\n", t.UTC().Format(time.UnixDate))
+	for _, p := range *places {
+		fmt.Fprintf(out, "%s: %s\n", p.Name, t.In(p.Location).Format(time.UnixDate))
+	}
 }
 
 func main() {
